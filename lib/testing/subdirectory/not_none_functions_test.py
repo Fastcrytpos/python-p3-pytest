1efe353d33faa9158e63777ec4a8f548810415fa
Samson Mubeya <rasspartah@gmail.com>
2024-05-24 18:07:41 +0300

    wrote and learnt writing tests

---
+++ lib/testing/subdirectory/not_none_functions_test.py
@@ -0,0 +1,5 @@
+from not_none_functions import return_not_none
+
+def test_return_not_none():
+    """this test checks and confirm that the function return_not_none in not_none_function.py does not return a none data type ie is not empty"""
+    assert return_not_none() != None
